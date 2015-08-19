3630dc21a76a0cfe957913021c88bff802f30836
stully2 <stully2@gmail.com>
2015-08-19 11:44:22 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The use of these two functions in conjunction allow a user to calculate once
+## and recall the inverse of a static square matrix. This will allow efficiency
+## in higher level coding by eliminating the need to recalculate on an as needed
+## basis.
+##------------------------------------------------------------------------------
 
-## Write a short comment describing this function
+## This function acts as an initializing function setting a 4 item list variable
+## example use:  a <- makeCacheMatrix(matix(rnorm(n*n),n,n)))
+##                       where n is a positive integer value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     i <- NULL 
+     set <- function(y) {
+            x <<- y
+            i <<- NULL
+     }
+     get <- function() x
+     setinverse <- function(inv) i <<- inv
+     getinverse <- function() i
+     list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Using this function will check for an exsisting inverse and return it.
+## Otherwise it will utalize the "solve" function and assign the resulting value
+## to the array created with the above function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+     ## Return a matrix that is the inverse of 'x'
+     i <- x$getinverse()
+     if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+     }
+     data <- x$get()
+     i <- solve(data)
+     x$setinverse(i)
+     i
 }
